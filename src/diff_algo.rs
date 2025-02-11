a3883aed5f5b8ff63ce47f54e5d739843f80b26e
inxeoz <pk@pk.com>
2025-02-11 20:49:19 +0530

    refactored restore previous version

--- src/diff_algo.rs
+++ src/diff_algo.rs
@@ -3,6 +3,7 @@ use crate::custom_error::ChakError;
 use crate::diff::deserialize_file_content;
 use crate::hashing::{hash_from_content, HashPointer};
 use crate::macros::save_or_create_file;
+use clap::builder::Str;
 use indexmap::{IndexMap, IndexSet};
 use itertools::{EitherOrBoth, Itertools};
 use serde::{Deserialize, Serialize};
@@ -14,7 +15,6 @@ use std::io;
 use std::io::{BufRead, BufReader, Write};
 use std::ops::Sub;
 use std::path::Path;
-use clap::builder::Str;
 
 #[derive(Serialize, PartialEq, Debug, Clone, Copy)]
 pub enum DiffLineType {
@@ -165,11 +165,10 @@ pub fn compare_hashed_content(
     let mut unique_line_contents = HashMap::<String, String>::new();
 
     for unique_prev_line_hash in unique_hash_lines_in_prev_lines {
-       if let Some(unique_line_content) = prev_line_contents.get(&unique_prev_line_hash) {
-           unique_line_contents.insert(unique_prev_line_hash.clone(), unique_line_content.clone());
-       }
+        if let Some(unique_line_content) = prev_line_contents.get(&unique_prev_line_hash) {
+            unique_line_contents.insert(unique_prev_line_hash.clone(), unique_line_content.clone());
+        }
     }
-
 
     HashedContent {
         pointer_to_previous_version: None,
@@ -186,47 +185,33 @@ pub fn file_to_lines(file: &File) -> Vec<String> {
         .collect()
 }
 
-//TODO use this function for making previous version file
-pub fn previous_content_from_new_content_using_diff(
-    diff: HashedContent,
-    fixed_next_content: HashedContent,
-) -> Vec<String> {
-    let mut previous_lines = Vec::<String>::new();
-    for line_hash in &diff.hash_lines {
-        let content = diff
+pub fn restore_previous_version(
+    fixed_next_content: &HashedContent,
+    diff_content: &HashedContent,
+) -> Result<Vec<String>, io::Error> {
+    let mut previous_lines = Vec::new();
+
+    for line_hash in &diff_content.hash_lines {
+        if let Some(content) = fixed_next_content
             .hash_to_content
             .get(line_hash)
-            .or_else(|| fixed_next_content.hash_to_content.get(line_hash))
-            .cloned()
-            .unwrap_or_default();
-
-        previous_lines.push(content);
+            .or_else(|| diff_content.hash_to_content.get(line_hash))
+        {
+            previous_lines.push(content.clone());
+        }
     }
 
-    previous_lines
-}
-
-pub fn restore_previous_version(blob_path: &Path, diff_path: &Path) -> Result<(), io::Error> {
-    let blob_file = File::open(blob_path)?;
-    let fixed_next_content = to_hashed_content(&blob_file);
-    let diff = deserialize_file_content(&diff_path)?;
-    let previous_version = previous_content_from_new_content_using_diff(diff, fixed_next_content);
-    let mut restore_file = save_or_create_file(
-        &get_project_dir().join("restore").join("file.txt"),
-        None,
-        true,
-    )?;
-
-    for lines in previous_version.iter() {
-        restore_file.write_all(lines.as_bytes())? // /n would work not here  ?
-    }
-
-    Ok(())
+    Ok(previous_lines)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::diff_algo::{compare_hashed_content, to_hashed_content};
+    use crate::config::get_project_dir;
+    use crate::diff::{deserialize_file_content, serialize_struct};
+    use crate::diff_algo::{
+        compare_hashed_content, restore_previous_version, to_hashed_content, HashedContent,
+    };
+    use crate::macros::save_or_create_file;
     use std::fs::File;
     use std::{env, io};
 
@@ -248,6 +233,41 @@ mod tests {
 
         let diff_biased_prev = compare_hashed_content(prev_file_content, new_file_content);
         println!("diff\n{}", serde_json::to_string_pretty(&diff_biased_prev)?);
+
+        let serialzed = serialize_struct(&diff_biased_prev);
+        save_or_create_file(
+            &get_project_dir().join("restore").join("diff.json"),
+            Some(&serialzed),
+            false,
+        )?;
+        Ok(())
+    }
+
+    #[test]
+    fn restore_previous_version_test() -> io::Result<()> {
+        //let diff_file = File::open(get_project_dir().join("restore").join("diff.json"))?;
+        let new_file = File::open(env::current_dir()?.join("file2.txt"))?;
+
+        // Generate mappings
+        let diff_file_content = deserialize_file_content::<HashedContent>(
+            &get_project_dir().join("restore").join("diff.json"),
+        )
+        .ok()
+        .expect("restore failed");
+        let new_file_content = to_hashed_content(&new_file);
+
+        if let Ok(diff_biased_prev) =
+            restore_previous_version(&new_file_content, &diff_file_content)
+        {
+            println!("diff\n{}", serde_json::to_string_pretty(&diff_biased_prev)?);
+
+            let serialzed = serialize_struct(&diff_biased_prev);
+            save_or_create_file(
+                &get_project_dir().join("restore").join("restored.json"),
+                Some(&serialzed),
+                false,
+            )?;
+        }
 
         Ok(())
     }
