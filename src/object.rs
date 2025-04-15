0b2ef3e4b3528b5cbcaf86cc42d01f57b55f99e5
inxeoz <inxeoz@gmail.com>
2025-04-15 13:46:07 +0530

    removed extra

--- src/object.rs
+++
@@ -1,6 +0,0 @@
-use std::path::PathBuf;
-pub trait ObjectTraits {
- //   type Output: HashPointerCommonTraits; // Ensures the returned type implements the trait
-    fn containing_folder() -> PathBuf;
-    // fn containing_folder_() -> PathBuf;
-}
